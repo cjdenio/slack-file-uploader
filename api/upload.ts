b9e556ae5e0112807d41db8665b6d145c4defe23
Caleb Denio <cjdenio44@gmail.com>
2020-07-02 09:19:28 -0400

    Tiny changes

--- api/upload.ts
+++ api/upload.ts
@@ -6,16 +6,17 @@ import { NowRequest, NowResponse } from "@vercel/node";
 export default async (req: NowRequest, res: NowResponse) => {
   if (req.query.token != process.env.CRON_AUTH_TOKEN) {
     res.status(401).send("Not authorized :(");
+    return;
   }
 
-  let file = await axios.get(<string>req.query.file, {
+  let file = await axios.get(req.query.file as string, {
     responseType: "stream",
   });
 
   let form = new FormData();
   form.append("token", process.env.SLACK_TOKEN);
-  form.append("channels", <string>req.query.channel);
-  form.append("initial_comment", <string>req.query.text);
+  form.append("channels", req.query.channel as string);
+  form.append("initial_comment", req.query.text as string);
   form.append("file", file.data);
   form.append("title", "image");
 
